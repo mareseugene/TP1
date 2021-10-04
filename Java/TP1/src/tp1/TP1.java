3174c9972f1025cd58e4af208a37836d79e50beb
egnma <egnma@192.168.115.184>
2021-10-04 11:26:14 +0200

    Ajout d'une variable

--- Java/TP1/src/tp1/TP1.java
+++ Java/TP1/src/tp1/TP1.java
@@ -14,7 +14,13 @@ public class TP1 {
      * @param args the command line arguments
      */ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        float temperature;
+        Scanner sc;
+        System.out.println("Veuillez saisir une valeur de type Float");
+        temperature = sc.nextFloat();
+        System.out.println("température : " + temperature);
     }
+
+    
     
 }
